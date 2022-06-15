b8bcf344bd3504149ff2ca770481bb6d77781ad5
George Nepharidze <you@example.com>
2022-06-15 03:48:03 +0200

    finished  type-aliases

---
+++ type-aliases/exercise-4-type-aliases-start.ts
@@ -0,0 +1,23 @@
+/**
+ * START: Follow the instructions below.
+ */
+
+// Create a `Country` type alias for the `string` type.
+// Change the type for each variable to use the `Country` type.
+
+type Country = string;
+
+const country1: Country = "Nigeria";
+
+const country2: Country = "Italy";
+
+const country3: Country = "China";
+
+console.log(typeof country1, country1)
+console.log(typeof country2, country2)
+console.log(typeof country3, country3)
+
+
+// ----
+
+export { };
